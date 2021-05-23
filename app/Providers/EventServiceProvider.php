a31e9a7c9a02d8737c0c0dddf8c8d702a85ece8b
vanhung <vanhungbkcbg1@gmail.com>
2021-05-23 15:21:59 +0700

    demo event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\AuctionUserWasOutbidded;
+use App\Listeners\EmailOutbiddedUser;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,6 +20,10 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        //dang ki event: format event=> array of listener
+        AuctionUserWasOutbidded::class=>[
+            EmailOutbiddedUser::class
+        ]
     ];
 
     /**
